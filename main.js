0cfddd2a5c4c773bc831a9a3bc223c6ef5f7f26f
Gabryel Alves <alvesgabryel086@gmail.com>
2023-10-01 13:58:33 -0300

    Separando funções
    separando em funções para serem chamadas

---
+++ main.js
@@ -0,0 +1,29 @@
+const form = document.getElementById('form-contatos');
+let linhas = '';
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    adicionalinha();
+
+});
+
+function adicionalinha() {
+    const inputNome = document.getElementById('nome-contato');
+    const inputNumero = document.getElementById('numero-tel');
+
+    let linha = '<tr>';
+    linha += `<td>${inputNome.value}</td>`;
+    linha += `<td>${inputNumero.value}</td>`;
+    linha += `</tr>`;
+
+    linhas += linha;
+
+    inputNome.value = ''
+    inputNumero.value = ''
+}
+
+function atualizatabela() {
+    const corpoTbody = document.querySelector('tbody');
+    corpoTbody.innerHTML = linhas 
+}
